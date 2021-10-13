af239cc4716b5828a60e2f604fdbdb9dfece394d
cleophas <cleophasmashiri@gmail.com>
2021-10-13 07:58:39 +0200

    First commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tictactoe'`, () => {
+  it(`should have as title 'Tic-Tac-Toe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tictactoe');
+    expect(app.title).toEqual('Tic-Tac-Toe');
   });
 
   it('should render title', () => {
